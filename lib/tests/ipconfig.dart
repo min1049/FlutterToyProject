ed4c06ab0ba88e8aae05f905c8a752e351f4ed78
yun-yeongmin <min1049@github.com>
2023-09-24 20:54:26 +0900

    Message issue : 0922
    to do
    1. update usrList

--- lib/tests/ipconfig.dart
+++ lib/tests/ipconfig.dart
@@ -1,6 +1,6 @@
 class IpConfig{
   //static const _URL = "http://10.14.4.159:8080/Room";
-  static const _URL = "http://10.14.4.163:8080/Room";
+  static const _URL = "http://101.101.218.10/Room";
   static const _URL_WS = "ws://10.14.4.163:8080/Room";
   static const _URL_NOPROXY = "http://170.20.10.2:8080/Room";
   String get getURL => _URL;
